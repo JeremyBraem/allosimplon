04b3749732e3ca22168bed425ed60b26dde9f9b4
JeremyBraem <braemjeremypro@gmail.com>
2023-03-16 16:21:18 +0100

    avancé

--- content/footer.php
+++ content/footer.php
@@ -5,9 +5,9 @@
         <a href="#"><img src="asset/img/email.png" class="w-10 md:w-16"></a>
         <a href="#"><img src="asset/img/facebook.png" class="w-10 md:w-16"></a>
     </div>
-    <div class="bg-[#FCFCFC] w-56 md:w-[1000px] h-[1px] m-auto my-10"></div>
+    <div class="bg-[#FCFCFC] w-56 md:w-[500px] lg:w-[1000px] h-[1px] m-auto my-10"></div>
     <a href="#"><p class="text-[#FCFCFC] text-xl">Politique de confidentialité</p></a>
     <a href="#"><p class="text-[#FCFCFC] text-xl mt-3">Politique de cookies</p></a>
-    <div class="bg-[#FCFCFC] w-56 md:w-[1000px] h-[1px] m-auto mt-10"></div>
+    <div class="bg-[#FCFCFC] w-56 md:w-[500px] lg:w-[1000px] h-[1px] m-auto mt-10"></div>
     <img src="asset/img/AlloSimplonBL.png" class="w-44 m-auto">
 </div>
--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     <!-- Contenu -->
     <section>
         <h2 class="bg-[#8666C6] text-center uppercase text-white text-2xl p-9">à la une</h2>
-        <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mt-10 md:mt-20 md:gap-x-6 gap-x-3 px-10 md:px-20">
+        <div class="relative grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mt-10 md:mt-20 md:gap-x-6 gap-x-3 px-10 md:px-20">
             <div class="bg-[#8666C6] rounded-sm overflow-hidden mb-4 h-44 md:h-96">
                 <a href="#">
                     <h3 class="text-xl text-center p-2 md:p-4 text-sm md:text-3xl text-white">Joker</h3>
@@ -78,6 +78,18 @@
                     <img src="asset/img/affiche-film-joker.jpg" class="rounded-b">
                 </a>
             </div>
+            <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg aria-hidden="true" class="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                <span class="sr-only">Previous</span>
+            </span>
+            </button>
+            <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true" class="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
         </div>
     </section>
     <section>
